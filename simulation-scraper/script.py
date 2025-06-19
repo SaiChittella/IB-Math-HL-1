e176e01a24ea8816a07cde4dfe9200db2a0c5b98
Sai Chittella <saichittella30@gmail.com>
2025-06-19 16:58:18 -0700

    Add functionality to scrape and process solar panel data; fix latitude input value

--- simulation-scraper/script.py
+++ simulation-scraper/script.py
@@ -3,8 +3,10 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+import math
 
 driver = webdriver.Chrome()
+all_rows = []
 
 try:
     driver.get("https://aa.usno.navy.mil/data/AltAz")
@@ -31,7 +33,7 @@ try:
         # For the latitude and longitude
         latitude_input =  wait.until(EC.presence_of_element_located((By.ID, "lat")))
         latitude_input.clear()
-        latitude_input.send_keys("42.60")
+        latitude_input.send_keys("47.60")
 
         longitude_input = wait.until(EC.presence_of_element_located((By.ID, "lon")))
         longitude_input.clear()
@@ -64,7 +66,12 @@ try:
         for row in rows:
             cells = row.find_elements(By.XPATH, ".//th|.//td")
             row_data = [cell.text.strip() for cell in cells]
-            print(row_data)
+
+            if len(row_data) == 3 and row_data[0] != 'Time':
+                if float(row_data[1][:-1]) < 0 or float(row_data[2][:-1]) < 0:
+                    continue
+                all_rows.append(row_data)
+
         
         driver.back()
 
@@ -72,5 +79,31 @@ try:
         print('\n')
 
 finally:
-    time.sleep(10) 
+    time.sleep(5) 
     driver.quit()
+
+
+panel_azimuth_degrees = 180
+panel_tilt_degrees = [0,4,7,9,11,12,15,17.5,19,20,21,22.5]
+
+for panel_tilt in panel_tilt_degrees:
+    panel_tilt_radians = math.radians(panel_tilt)
+
+    cos_theta_sum = 0.0
+
+    for row in all_rows:
+        # Angle of incidence
+        sun_altitude_str = row[1]
+        sun_altitude = float(sun_altitude_str[:-1]) 
+
+        sun_azimuth_str = row[2]
+        sun_azimuth = float(sun_azimuth_str[:-1])
+
+        cos_theta_i = (math.sin(math.radians(sun_altitude)) * math.cos(panel_tilt_radians)) + (math.cos(math.radians(sun_altitude)) * math.sin(panel_tilt_radians) * math.cos(math.radians(sun_azimuth - panel_azimuth_degrees)))
+
+        cos_theta_sum += cos_theta_i
+
+    print(f'{panel_tilt}° Incidence Sum: {cos_theta_sum}')
+
+    energy = cos_theta_sum * 1.6 * 0.2
+    print(f'{panel_tilt} Energy Sum: {energy} \n') 
